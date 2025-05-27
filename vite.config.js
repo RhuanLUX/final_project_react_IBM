b7ee8f309c70a916870585f0bea95997c83a2ff2
Rhuan Pablo <160653297+RhuanLUX@users.noreply.github.com>
2025-05-27 16:19:41 +0200

    Update vite.config.js
    manual change in the repository name;

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/final_project_react_IBM",
+    base: "/learning_react_IBM",
   plugins: [react()],
   
 })
